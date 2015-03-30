a97ad057c4a538fd654cdabddafc95463a4194c7
ducbaovn <ducbaovn@gmail.com>
2015-03-30 20:13:13 +0700

    Update configAuth.js
    remove my secret info

--- configAuth.js
+++ configAuth.js
@@ -1,13 +1,13 @@
 module.exports = {
     'facebookAuth' : {
-        'clientID'      : '701418339980222', // your App ID
-        'clientSecret'  : '24bd02121bb501bb8e3b2c910395e97c', // your App Secret
+        'clientID'      : '', // your App ID
+        'clientSecret'  : '', // your App Secret
         'callbackURL'   : 'http://localhost:8000/auth/facebook/callback'
     },
 
     'googleAuth' : {
-        'clientID'      : '235462051801-dpg820t6b9rs7lrdp70ksqnrdiv5qept.apps.googleusercontent.com',
-        'clientSecret'  : 'GHU_A4jKjB3oqPs__TMRrQk2',
+        'clientID'      : '',
+        'clientSecret'  : '',
         'callbackURL'   : 'http://localhost:8000/auth/google/callback'
     }
 
